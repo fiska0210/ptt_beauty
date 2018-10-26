31e5775c01d0231311fcbbbf8d6bb98cf66a2b20
fiska0210 <asdzxc2671@gmail.com>
2018-10-26 12:29:19 +0800

    send2line

--- tutorial4_demo.py
+++ tutorial4_demo.py
@@ -5,6 +5,9 @@ import os
 import re
 import urllib.request
 import json
+import shutil
+import sys
+import codecs
 
 
 PTT_URL = 'https://www.ptt.cc'
@@ -35,6 +38,7 @@ def get_articles(dom, date):
     for d in divs:
         if d.find('div', 'date').string.strip() == date:  # check the posting date
             # get the number of 'push'
+            global push_count 
             push_count = 0
             if d.find('div', 'nrec').string:
                 try:
@@ -64,11 +68,11 @@ def parse(dom):
     return img_urls
 
 
-def save(img_urls, title, counter):
+def save(img_urls, title, times, datapath): #nooooot yet!
     if img_urls:
         try:
             #dname = title.strip()  #  use strip() to delete the space' ' in the front and end of string
-            dname = 'testfile' + str(counter)
+            dname = 'file' + str(times)
             os.makedirs(dname)
             for img_url in img_urls:
                 if img_url.split('//')[1].startswith('m.'):
@@ -79,32 +83,106 @@ def save(img_urls, title, counter):
                     img_url += '.jpg'
                 fname = img_url.split('/')[-1]
                 urllib.request.urlretrieve(img_url, os.path.join(dname, fname))
+            read_floder(datapath,times)
         except Exception as e:
             print(e)
+    print("final done!!!")
+
+
+def lineNotify(msg, picURI):
+    url = "https://notify-api.line.me/api/notify"
+    headers = {
+        "Authorization": "Bearer " + "06OrDKN9pqYaTIe7dYaA26bmiStbUCRvzDc5ZVulCNT"
+    }
+    #line_api_test : 06OrDKN9pqYaTIe7dYaA26bmiStbUCRvzDc5ZVulCNT
+   
+    payload = {'message': msg}
+    files = {'imageFile': open(picURI, 'rb')}
+    r = requests.post(url, headers = headers, params = payload, files = files)
+    #r = requests.post(url, headers = headers, files = files)
+    return r.status_code
+    print("api done!")
+ 
+def read_floder(datapath,times):
+    print("read fresh folder!")
+    print(datapath)
+
+    for dirname, dirnames, filenames in os.walk('.'):
+        for f in filenames:
+            #print("times :" + str(dirname[7]))
+            if str(dirname[2:7]) == "file"+str(times) :
+                print("tset " + str(dirname[2:7]))
+                # if dirname[7] == time : 
+                filepath = str(dirname) + "/" + str(f)
+                print(filepath)
+                msg = 'https://www.ptt.cc/' + str(datapath)
+                lineNotify(msg, filepath)
+
+                print("send to line!") 
 
 
 if __name__ == '__main__':
-    current_page = get_web_page('https://www.ptt.cc/bbs/Sex/index.html')
-    folder_counter = 0
+    sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+    current_page = get_web_page('https://www.ptt.cc/bbs/Beauty/index.html')
+    token = "06OrDKN9pqYaTIe7dYaA26bmiStbUCRvzDc5ZVulCNT"
+    times = 0
+    read = []
+    with open('record.txt') as f:
+        for each_line in f :
+                #a = each_line.split("\n")
+                #read.append(str(each_line.split("\n")))
+            read = each_line.split("\n")
+        print(each_line)
+        print(read[0])
+    
+
     if current_page:
         articles = []  # all articles today
         date = time.strftime("%m/%d").lstrip('0')  # date of today, remove '0' in front to match format of PTT 
-        print("today is : " + date)        
+        print(date)
+        # date = '10/12'        
         current_articles, prev_url = get_articles(current_page, date)  # today's articles in current page
+        
         while current_articles:  # if adding today's articles in current page, back to previous pages and keeping search weather exist today's argicles
             articles += current_articles
             current_page = get_web_page(PTT_URL + prev_url)
             current_articles, prev_url = get_articles(current_page, date)
+           
 
         # got article-list and starting enter to each article and imgread
-        for article in articles:
-            print('Processing', article)
-            folder_counter = folder_counter + 1
-            page = get_web_page(PTT_URL + article['href'])
-            if page:
-                img_urls = parse(page)
-                save(img_urls, article['title'], folder_counter)
-                article['num_image'] = len(img_urls)
+        with open('record.txt', 'w') as w :
+            for i in range(len(read)) : 
+                w.write(read[i])
+                #w.write("\n")
+
+            for article in articles:
+                datapath = article['href']
+                push_counter = article['push_count']
+                print(datapath)
+                print(push_counter)
+                if datapath not in read : 
+                    w.write(datapath)
+                    w.write("\n")
+                    print('Processing', article)
+                    
+                    # datapath = article['href']
+                    #rprint(datapath)
+                    
+                    
+                    page = get_web_page(PTT_URL + article['href'])
+                    # print(page)
+                    if page:
+                        img_urls = parse(page)
+                        if push_counter > 10 :
+                            print("push = " + str(push_counter))
+                            save(img_urls, article['title'], times, datapath)
+                            # print("title")
+                            # print(article['title'])
+                            times = times + 1
+                            article['num_image'] = len(img_urls)
+                        else : 
+                           continue
+
 
         # store article data
         with open('data.json', 'w', encoding='utf-8') as f:
